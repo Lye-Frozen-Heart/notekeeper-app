c10fd4b513a5929729f19b38e8650b510ab552a5
lye <torresbravoalex@gmail.com>
2024-05-20 18:40:08 +0200

    Developed validator and given test functions

---
+++ src/helpers/validation/test/validateType.test.js
@@ -0,0 +1,14 @@
+import { expect, test } from 'vitest';
+import { validateType } from '../validateType';
+
+test('validateType returns true for correct type', () => {
+  const value = 123;
+  const type = 'number';
+  expect(validateType(value, type)).toBe(true);
+});
+
+test('validateType returns false for incorrect type', () => {
+  const value = 'abc';
+  const type = 'number';
+  expect(validateType(value, type)).toBe(false);
+});
