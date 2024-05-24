98472f67171e032d93074ce40406afc859918b76
lye <torresbravoalex@gmail.com>
2024-05-24 16:55:12 +0200

    Added comments to test and validator functions. Also fixed a Script

--- src/helpers/validation/test/validateNonEmpty.test.js
+++ src/helpers/validation/test/validateNonEmpty.test.js
@@ -1,12 +1,13 @@
-import { expect, test } from 'vitest';
-import { validateNonEmpty } from '../validateNonEmpty';
+//Non AI was used to develop the given tests
+import { expect, test } from "vitest";
+import { validateNonEmpty } from "../validateNonEmpty";
 
-test('validateNonEmpty returns true for non-empty value', () => {
-  const value = 'hello';
+test("validateNonEmpty returns true for non-empty value", () => {
+  const value = "hello";
   expect(validateNonEmpty(value)).toBe(true);
 });
 
-test('validateNonEmpty returns false for empty value', () => {
-  const value = '';
+test("validateNonEmpty returns false for empty value", () => {
+  const value = "";
   expect(validateNonEmpty(value)).toBe(false);
 });
--- src/helpers/validation/test/validateType.test.js
+++ src/helpers/validation/test/validateType.test.js
@@ -1,14 +1,15 @@
-import { expect, test } from 'vitest';
-import { validateType } from '../validateType';
+//Non AI was used to develop the given tests
+import { expect, test } from "vitest";
+import { validateType } from "../validateType";
 
-test('validateType returns true for correct type', () => {
+test("validateType returns true for correct type", () => {
   const value = 123;
-  const type = 'number';
+  const type = "number";
   expect(validateType(value, type)).toBe(true);
 });
 
-test('validateType returns false for incorrect type', () => {
-  const value = 'abc';
-  const type = 'number';
+test("validateType returns false for incorrect type", () => {
+  const value = "abc";
+  const type = "number";
   expect(validateType(value, type)).toBe(false);
 });
